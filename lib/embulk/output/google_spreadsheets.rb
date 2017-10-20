b85a93adb2f01d8ade9a5c28039322618a767cb2
ryuji.ito <ryuji.ito@dena.com>
2017-10-20 16:26:21 +0900

    rm all

--- lib/embulk/output/google_spreadsheets.rb
+++
@@ -1,3 +0,0 @@
-Embulk::JavaPlugin.register_output(
-  "google_spreadsheets", "org.embulk.output.google_spreadsheets.GoogleSpreadsheetsOutputPlugin",
-  File.expand_path('../../../../classpath', __FILE__))
